08fbe1004062f2492dab9ef3d3e075214923a15a
Andy Wang <ownz.andy@gmail.com>
2017-06-11 03:42:25 -0700

    chat UI

--- public/js/index.js
+++ public/js/index.js
@@ -12,6 +12,7 @@ var userContext = { identity: null, endpoint: null };
 let AUTHOR = "ownz.andy@gmail.com";
 
 $(document).ready(function() {
+
   $('#login-name').focus();
 
   $('#login-button').on('click', function() {
@@ -397,28 +398,18 @@ function updateMessage(message) {
 }
 
 function createMessage(message, $el) {
-  var $remove = $('<div class="remove-button glyphicon glyphicon-remove"/>')
-    .on('click', function(e) {
-      e.preventDefault();
-      message.remove();
-    }).appendTo($el);
-
-  var $edit = $('<div class="remove-button glyphicon glyphicon-edit"/>')
-    .on('click', function(e) {
-      e.preventDefault();
-      $('.body', $el).hide();
-      $('.edit-body', $el).show();
-      $('button', $el).show();
-      $el.addClass('editing');
-    }).appendTo($el);
 
   if(message.state.author === AUTHOR) {
-    var $img = $('<img/>')
-    .attr('src', 'http://gravatar.com/avatar/' + MD5(message.author) + '?s=30&d=mm&r=g')
-    .appendTo($el);
 
     var $author = $('<p class="author"/>')
       .appendTo($el);
+        var $body = $('<p class="channel-message-body-right"/>')
+    .text(message.body)
+    .appendTo($el);
+
+        var $img = $('<img class="avatar-right"/>')
+    .attr('src', 'http://gravatar.com/avatar/' + MD5(message.author) + '?s=30&d=mm&r=g')
+    .appendTo($el);
   }    
 
   var time = message.timestamp;
@@ -427,11 +418,11 @@ function createMessage(message, $el) {
 
   if (minutes < 10) { minutes = '0' + minutes; }
 
-  var $body = $('<p class="body"/>')
+if(message.state.author !== AUTHOR) {
+    var $body = $('<p class="channel-message-body"/>')
     .text(message.body)
     .appendTo($el);
-if(message.state.author !== AUTHOR) {
-    var $img = $('<img/>')
+    var $img = $('<img class="avatar"/>')
     .attr('src', 'http://gravatar.com/avatar/' + MD5(message.author) + '?s=30&d=mm&r=g')
     .appendTo($el);
 
@@ -439,28 +430,7 @@ if(message.state.author !== AUTHOR) {
     .appendTo($el);
 }
 
-  var $editBody = $('<textarea class="edit-body"/>')
-    .text(message.body)
-    .appendTo($el);
 
-  var $cancel = $('<button class="cancel-edit"/>')
-    .text('Cancel')
-    .on('click', function(e) {
-      e.preventDefault();
-      $('.edit-body', $el).hide();
-      $('button', $el).hide();
-      $('.body', $el).show();
-      $el.removeClass('editing');
-    }).appendTo($el);
-
-  var $edit = $('<button class="red-button"/>')
-    .text('Make Change')
-    .on('click', function(e) {
-      message.updateBody($editBody.val());
-    }).appendTo($el);
-
-  var $membersRead = $('<p class="members-read"/>')
-    .appendTo($el);
 }
 
 function prependMessage(message) {
@@ -592,6 +562,8 @@ function setActiveChannel(channel) {
   }
 
   activeChannel = channel;
+
+  $('#message-body-input').attr("placeholder", 'Send a message to ' + channel.friendlyName)
 
   $('#channel-title').text(channel.friendlyName);
   $('#channel-messages ul').empty();
